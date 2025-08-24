0f7b6190f37d33365a1f097d8b699fedb7110283
jan-natt <jannatulferdous139997@gmail.com>
2025-08-25 04:25:48 +0600

    up

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -17,8 +17,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique(); // Added phone number
             $table->string('password');
+            $table->string('nid_number')->unique()->nullable(); // Added NID number
+            $table->string('nid_verification_photo')->nullable(); // NID verification photo path
+            $table->string('user_photo')->default('default.png'); // User profile photo
             $table->rememberToken();
             $table->timestamps();
         });
@@ -33,4 +36,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
